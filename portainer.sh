553f053017047337af17f1e96350a3e24da94e5e
Nuno Gonçalves <nuno.gonkalves@gmail.com>
2021-01-10 17:26:21 +0000

    Hash portainer password

--- portainer.sh
+++ portainer.sh
@@ -19,16 +19,27 @@ create|build)
         echo -e "Creating container $CONTAINER\n"
         echo -e "---------------------------------\n"
 
+        HTPASSWD_IMAGE="httpd:alpine"
+
+        docker pull "$HTPASSWD_IMAGE"
+
+        PASSWORD_HASH=$(docker run --rm "$HTPASSWD_IMAGE" htpasswd -nbB admin "$PASSWORD" | cut -d ":" -f 2)
+
+        docker rmi -f $(docker images --format '{{.ID}}' --filter 'reference=$HTPASSWD_IMAGE')
+
         docker create \
             --name="$CONTAINER" \
             -e TZ="$TIMEZONE" \
             -e PUID=$PUID \
             -e PGID=$PGID \
-            -e SERVERURL=$SERVERURL \
+            -v /etc/localtime:/etc/localtime:ro \
+            -v /var/run/docker.sock:/var/run/docker.sock:ro \
             -v $VOL_DATA:/data \
             --restart="$CONTAINER_RESTART_MODE" \
             -p $PORT:9000 \
-            "$IMAGE"
+            "$IMAGE" \
+            -H unix:///var/run/docker.sock \
+            --admin-password="$PASSWORD_HASH"
         exit $?
         ;;
 *)
