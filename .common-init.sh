3d6216a4cc744d3990b66c54c9af1071df03a98e
Nuno Gonçalves <nuno.gonkalves@gmail.com>
2021-01-12 19:30:39 +0000

    Added container name prefix to better identify containers from these scripts

--- .common-init.sh
+++ .common-init.sh
@@ -4,9 +4,11 @@ CURRENT_FILE=$SCRIPT
 
 FILE_VARS="${CURRENT_FILE%%.*}.vars"
 
-
 # load file with variables if exists
 [[ -e $FILE_VARS ]] && source $FILE_VARS
 
 [[ ! -e $FILE_VARS ]] && echo -e "Variables file doesn't exist: $FILE_VARS\n\nRename the $FILE_VARS.template to $FILE_VARS as starting point.\n" && exit 1
 
+# add docker container prefix
+CONTAINER_PREFIX="ds"
+CONTAINER="${CONTAINER_PREFIX}-${CONTAINER}"
