593889e3113faf51ed3891e079bbb50eec80892c
Nuno Gonçalves <nuno.gonkalves@gmail.com>
2021-02-21 01:07:33 +0000

    Default configuration is now set by default by .conf files To override default properties a new file must be created with name .override.conf

--- .common-init.sh
+++ .common-init.sh
@@ -2,12 +2,18 @@ CURRENT_DIR=$SCRIPTPATH
 
 CURRENT_FILE=$SCRIPT
 
-FILE_VARS="${CURRENT_FILE%%.*}.vars"
+FILE_NO_EXTENSION="${CURRENT_FILE%%.*}"
+
+FILE_VARS="${FILE_NO_EXTENSION}.conf"
+
+OVERRIDE_FILE_VARS="${FILE_NO_EXTENSION}.override.conf"
 
 # load file with variables if exists
 [[ -e $FILE_VARS ]] && source $FILE_VARS
 
-[[ ! -e $FILE_VARS ]] && echo -e "Variables file doesn't exist: $FILE_VARS\n\nRename the $FILE_VARS.template to $FILE_VARS as starting point.\n" && exit 1
+[[ ! -e $FILE_VARS ]] && echo -e "Variables file doesn't exist: $FILE_VARS" && exit 1
+
+[[ -e $OVERRIDE_FILE_VARS ]] && echo "Loading override file: $OVERRIDE_FILE_VARS" && source $OVERRIDE_FILE_VARS
 
 # add docker container prefix
 CONTAINER_PREFIX="ds"
